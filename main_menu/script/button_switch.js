b22bd8ed64c02ffb15f8fd405a62548bea423ab8
7Prince11 <fazzytron@gmail.com>
2022-06-29 01:08:22 +0200

    Main page

---
+++ main_menu/script/button_switch.js
@@ -0,0 +1,23 @@
+const button = document.querySelectorAll('button');
+ 
+
+for (let i = 0; i < button.length; i++) {
+    button[0].addEventListener("click",  () => {
+        location.href = '../../sport_api/index.html';
+    });
+    button[1].addEventListener("click", () => {
+        location.href = '../../time_zone_api/index.html';
+    })
+    button[2].addEventListener("click", () => {
+        location.href = '../../weather_rn/index.html';
+    })
+    button[3].addEventListener("click", () => {
+        location.href = '../../astronomy/index.html';
+    })
+
+
+}
+
+
+
+ 
